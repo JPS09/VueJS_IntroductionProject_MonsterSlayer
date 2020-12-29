c60178abcd8de71f0b9af1c89059b3f40ed0fd12
Jean Pierre Symoens <jpsymoens0@gmail.com>
2020-12-29 10:49:39 +0100

    Same with ternary operator

--- app.js
+++ app.js
@@ -12,21 +12,16 @@ const app = Vue.createApp({
   methods: {
     attackMonster() {
       const attackValue = getRandomValue(5, 12);
-      if ((this.monsterHealth -= attackValue) < 0) {
-        this.monsterHealth = 0;
-      } else {
-        this.monsterHealth -= attackValue;
-      }
+      (this.playerHealth -= attackValue) < 0
+        ? (this.playerHealth = 0)
+        : (this.playerHealth -= attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
-      if ((this.playerHealth -= attackValue) < 0) {
-        this.playerHealth = 0;
-      } else {
-        this.playerHealth -= attackValue;
-      }
-      console.log(this.playerHealth);
+      (this.playerHealth -= attackValue) < 0
+        ? (this.playerHealth = 0)
+        : (this.playerHealth -= attackValue);
     },
   },
   computed: {
